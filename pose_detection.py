f07a294783eb52cfb0eb1c32a97aac1607ae538f
Rodrigo <rodrigot@videotron.ca>
2025-12-21 03:21:49 -0500

    extraction of result from async

--- pose_detection.py
+++ pose_detection.py
@@ -3,26 +3,71 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-#Video capture
-capture = cv2.VideoCapture(0)
+#Class to excract the results from the async callback
+class LandmarkerResult:
+    def __init__(self):
+        self.result = None
+
+    #fuction to callback to print results
+    def print_result(self, result: mp.tasks.vision.PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+        self.result = result
+#    print(f'pose landmarker result: {result}')
+    
+
+# Initialize live camera feed with openCV
+capture = cv2.VideoCapture(0)#TODO : change the index if you have multiple cameras
 if not capture.isOpened():
     print("Error: Could not open camera.")
     exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = capture.read()
 
-    #if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) == ord('q'):
-        break
+#initializing task for model
+BaseOptions = mp.tasks.BaseOptions
+PoseLandmarker = mp.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+result = LandmarkerResult()
+
+options = PoseLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path="pose_landmarker_lite.task"),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=result.print_result)
+
+# Create a pose landmarker instance with the live stream mode:
+with PoseLandmarker.create_from_options(options) as landmarker:
+   
+    #Start the camera feed loop
+    while capture.isOpened():
+        frame_timestamp_ms = int(capture.get(cv2.CAP_PROP_POS_MSEC))
+
+        # Capture frame-by-frame
+        ret, cameraFeed = capture.read()
+
+        #coloring conversion for mediapipe
+        feedForDetection = cv2.cvtColor(cameraFeed, cv2.COLOR_BGR2RGB)
+
+        #Transform the OpenCV image to mediapipe image format
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=feedForDetection)  
+        # Use the landmarker to detect poses in the input camera feed.
+        landmarker.detect_async(mp_image, frame_timestamp_ms)
+        print(result.result)
+        
+        #Change the color back to BGR for OpenCV
+        cameraFeed = cv2.cvtColor(feedForDetection, cv2.COLOR_RGB2BGR)
+        #mp_drawing.draw_landmarks(cameraFeed, detectionResult.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        # Display the resulting frame
+        cv2.imshow('Camera Feed', cameraFeed)
+
+        #exit on 'q' key press
+        if cv2.waitKey(1) == ord('q'):
+            break
+
 # When everything done, release the capture
 capture.release()
 cv2.destroyAllWindows()
+
+
+
+
 
